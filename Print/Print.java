7dd59fd368ee03c0e943f65a199a70ea7fa74a15
Aarya Goriya <aaryagoriya@gmail.com>
2025-08-18 17:46:16 +1200

    Update Print.java

--- Print/Print.java
+++ Print/Print.java
@@ -1,3 +1,9 @@
+// public class Print: Defines a class named Print.
+
+// public static void main(String[] args): The main method where the program starts.
+
+// System.out.println("Hello World");: Prints the text "Hello World" to the console.
+
 public class Print {
     public static void main(String[] args) {
         System.out.println("Hello World");
