f57e90723a294adaa9f984a7a3ef7ae9f826edc2
Aarya Goriya <aaryagoriya@gmail.com>
2025-08-20 14:06:53 +1200

    Create Datatypes.java

---
+++ Datatypes/Datatypes.java
@@ -0,0 +1,10 @@
+package JAVA.Datatypes;
+// DATATYPES
+// Interger
+// int is used for integers to define datatype of a variable.
+public class Datatypes {
+    public static void main(String[] args) {
+         int age = 25;
+         System.out.println(age);
+    }
+}
