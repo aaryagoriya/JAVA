0b901172a909d4f39f72afd728a095f6373e2f6f
Aarya Goriya <aaryagoriya@gmail.com>
2025-08-22 15:26:30 +1200

    Create Float.java

---
+++ Datatypes/Float.java
@@ -0,0 +1,9 @@
+// FLOAT
+// float in Java is a data type used to store decimal numbers with single precision (less accurate than double).
+
+public class Float {
+    public static void main(String[] args) {
+        float c = 3.14f;
+        System.out.println(c);
+    }
+}
